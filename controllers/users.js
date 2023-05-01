451bb5817a1b2d3dc1bfb1261d15d64a19db9476
Leonid Rublevskiy <leops@inbox.ru>
2023-05-01 19:59:20 +0300

    -amend

--- controllers/users.js
+++ controllers/users.js
@@ -1,40 +1,25 @@
-const {
-  CastError,
-  ValidationError,
-} = require('mongoose').Error;
-
 const User = require('../models/user');
+const { errorsProcessing } = require('../utils/utils');
 
 const CODE = 200;
 const CREATED_CODE = 201;
-const ERROR_BAD_REQUEST_CODE = 400;
+// const ERROR_BAD_REQUEST_CODE = 400;
 
-const ERROR_NOT_FOUND_CODE = 404;
-const ERROR_SERVER_CODE = 500;
+// const ERROR_NOT_FOUND_CODE = 404;
+// npconst ERROR_SERVER_CODE = 500;
 
 //= ====================================================
 module.exports.getAllUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(CODE).send(users))
-    .catch(() => res.status(ERROR_SERVER_CODE)
-      .send({ message: `Ошибка сервера ${ERROR_SERVER_CODE}` }));
+    .catch((err) => errorsProcessing(err, res));
 };
 //= ====================================================
 module.exports.getUserId = (req, res) => {
   User.findById(req.params.userId)
     .orFail()
     .then((user) => res.send(user))
-    .catch((err) => {
-      if (err instanceof CastError) {
-        return res
-          // .send(console.log('res: '))
-          // .send(console.log({res}))
-          .status(ERROR_BAD_REQUEST_CODE)
-          .send({ message: `Ошибка загрузки ${ERROR_BAD_REQUEST_CODE}` });
-      }
-      return res.status(ERROR_NOT_FOUND_CODE)
-        .send({ message: `страница не найдена ${ERROR_NOT_FOUND_CODE}` });
-    });
+    .catch((err) => errorsProcessing(err, res));
 };
 //= ====================================================
 
@@ -43,14 +28,7 @@ module.exports.createUser = (req, res) => {
   User.create({ name, about, avatar })
     .then((user) => res.status(CREATED_CODE)
       .send(user))
-    .catch((err) => {
-      if (err instanceof ValidationError) {
-        return res.status(ERROR_BAD_REQUEST_CODE)
-          .send({ message: `Ошибка загрузки ${ERROR_BAD_REQUEST_CODE}` });
-      }
-      return res.status(ERROR_SERVER_CODE)
-        .send({ message: `Ошибка сервера ${ERROR_SERVER_CODE}` });
-    });
+    .catch((err) => errorsProcessing(err, res));
 };
 //= ====================================================
 
@@ -61,39 +39,20 @@ module.exports.updateUser = (req, res) => {
     runValidators: true,
     new: true,
   })
+    .orFail()
     .then((user) => res.send(user))
-    .catch((err) => {
-      if (err instanceof ValidationError) {
-        return res
-          .status(ERROR_BAD_REQUEST_CODE)
-          .send({
-            message: `Некорректные данные ${ERROR_BAD_REQUEST_CODE}`,
-          });
-      }
-      return res.status(ERROR_BAD_REQUEST_CODE).send({
-        message: `Введены некорректные данные ${ERROR_BAD_REQUEST_CODE}`,
-      });
-    });
+    .catch((err) => errorsProcessing(err, res));
 };
 //= ====================================================
 
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
   const userId = req.user._id;
-  User.findByIdAndUpdate(
-    userId,
-    { avatar }, // параметры необходимо передавать в объекте
-    { runValidators: true, new: true },
-  )
+  User.findByIdAndUpdate(userId, avatar, {
+    runValidators: true,
+    new: true,
+  })
+    .orFail()
     .then((user) => res.send(user))
-    .catch((err) => {
-      if (err instanceof ValidationError) {
-        return res.status(ERROR_BAD_REQUEST_CODE).send({
-          message: `Некорректные данные ${ERROR_BAD_REQUEST_CODE}`,
-        });
-      }
-      return res.status(ERROR_SERVER_CODE).send({
-        message: `Ошибка сервера ${ERROR_SERVER_CODE}`,
-      });
-    });
+    .catch((err) => errorsProcessing(err, res));
 };
