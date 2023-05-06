73f5b137267fe23fb3e92e4962afd80dc85469e4
Leonid Rublevskiy <leops@inbox.ru>
2023-05-06 18:20:51 +0300

    add in schema

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -17,6 +19,34 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    unique: true,
+    require: true,
+    validate: {
+      validator: (email) => validator.isEmail(email),
+      message: ({ val }) => `${val} Недействительный адрес`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
 });
+
+userSchema.statics.findUserByCredentials = function noNameFunc(email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильная почта или пароль'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (matched) { return (user); }
+          return Promise.reject(new Error('Неправильная почта или пароль'));
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
